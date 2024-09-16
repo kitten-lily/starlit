a191c68b7798c8fba5880db96b3b4781643998df
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-09-16 08:45:26 +0000

    Oops

--- build_files/bluefin-files.sh
+++ build_files/bluefin-files.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-git clone git clone https://github.com/ublue-os/bluefin.git /tmp/bluefin
+git clone https://github.com/ublue-os/bluefin.git /tmp/bluefin
 
 mkdir -p /tmp/system_files/etc
 mv /tmp/bluefin/system_files/shared/etc/yum.repos.d /tmp/system_files/etc/
